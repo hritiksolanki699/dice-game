2cdfe33424773472e9179aa19be0384e9dce4bc1
Hritik <hritiksolanki699@gmail.com>
2022-05-27 09:08:20 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber= Math.floor(Math.random()*6)+1; //1=6
+var randomImageSource="images/dice" + randomNumber+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomImageSource2="images/dice" + randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if (randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML="palyer 1 wins";
+}else if( randomNumber<randomNumber2){
+    document.querySelector("h1").innerHTML="palyer 2 wins";
+}else{
+    document.querySelector("h1").innerHTML="draw";
+}
